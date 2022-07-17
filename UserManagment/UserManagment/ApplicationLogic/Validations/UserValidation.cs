40a9f4b696adb3ca458f68e519dbabe7d3deddd8
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 22:10:50 +0400

    IsValidEmail and IsUserExist methods added to UserValidation class

--- UserManagment/UserManagment/ApplicationLogic/Validations/UserValidation.cs
+++ UserManagment/UserManagment/ApplicationLogic/Validations/UserValidation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagment.DataBase.Repo;
 
 namespace UserManagment.ApplicationLogic.Validations
 {
@@ -32,6 +33,33 @@ namespace UserManagment.ApplicationLogic.Validations
                 " make sure the name contains only letters," +
                 " the first letter is uppercase," +
                 " and the length is greater than 3 and less than 30");
+
+            return false;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (Validation.IsConsistEmail(email))
+            {
+                return true;
+            }
+            Console.WriteLine("Receipent- can consist of only letters and numbers," +
+                " the total length can be min 10 max 30," +
+                "Separator - there must be an @ in between," +
+                "Domain - can only end with code.edu.az," +
+                "Example: mahmood@code.edu.az");
+
+            return false;
+        }
+
+        public static bool IsUserExist(string email)
+        {
+            if (UserRepo.IsUserExistsByEmail(email))
+            {
+                Console.WriteLine("User already exists");
+
+                return true;
+            }
 
             return false;
         }
