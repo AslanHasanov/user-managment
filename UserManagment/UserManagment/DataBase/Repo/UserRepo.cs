034ceba0dbe7dcf0ef8da562bad526950119ea4f
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 21:55:44 +0400

    IsUserExistsByEmail and GetUserByEmailAndPassword methods added to UserRepo class

--- UserManagment/UserManagment/DataBase/Repo/UserRepo.cs
+++ UserManagment/UserManagment/DataBase/Repo/UserRepo.cs
@@ -12,7 +12,6 @@ namespace UserManagment.DataBase.Repo
         public static List<User> Users { get; set; } = new List<User>()
         {
             new User("Admin","Super","admin@gmail.com","123321")
-
         };
 
         public static User AddUser(string firstName, string lastName, string email, string password)
@@ -20,6 +19,32 @@ namespace UserManagment.DataBase.Repo
             User user = new User(firstName, lastName, email, password);
             Users.Add(user);
             return user;
+        }
+
+        public static bool IsUserExistsByEmail(string email)
+        {
+            foreach (User user in Users)
+            {
+                if (user.Email == email)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static User GetUserByEmailAndPassword(string email, string password)
+        {
+            foreach (User user in Users)
+            {
+                if (user.Email == email && user.Password == password)
+                {
+                    return user;
+                }
+            }
+
+            return null;
         }
     }
 }
