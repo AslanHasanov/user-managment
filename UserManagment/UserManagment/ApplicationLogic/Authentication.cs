8ed034488fc97749b17794404fbe5dc2495b2832
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 22:18:07 +0400

    GetFirstName method added to Authentication class

--- UserManagment/UserManagment/ApplicationLogic/Authentication.cs
+++ UserManagment/UserManagment/ApplicationLogic/Authentication.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagment.ApplicationLogic.Validations;
 
 namespace UserManagment.ApplicationLogic
 {
     internal class Authentication
     {
+        public static string GetFirstName()
+        {
+            Console.Write("Please enter your name : ");
+            string firstName = Console.ReadLine();
+
+            while (!UserValidation.IsValidFirstName(firstName))
+            {
+                Console.Write("Please enter correct name : ");
+                firstName = Console.ReadLine();
+            }
+
+            return firstName;
+        }
     }
 }
