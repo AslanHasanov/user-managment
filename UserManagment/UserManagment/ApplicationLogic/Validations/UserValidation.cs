ee6faea2183f6255cff573034f3c5d9b5c4d9075
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 22:08:05 +0400

    IsValidFirstName and IsValidLastName methods added to UserValidation class

--- UserManagment/UserManagment/ApplicationLogic/Validations/UserValidation.cs
+++ UserManagment/UserManagment/ApplicationLogic/Validations/UserValidation.cs
@@ -8,5 +8,32 @@ namespace UserManagment.ApplicationLogic.Validations
 {
     internal class UserValidation
     {
+        public static bool IsValidFirstName(string firstName)
+        {
+            if (Validation.IsValidText(firstName))
+            {
+                return true;
+            }
+            Console.WriteLine("Entered name is incorrect," +
+                " make sure the name contains only letters," +
+                " the first letter is uppercase," +
+                " and the length is greater than 3 and less than 30");
+
+            return false;
+        }
+
+        public static bool IsValidLastName(string lastName)
+        {
+            if (Validation.IsValidText(lastName))
+            {
+                return true;
+            }
+            Console.WriteLine("Entered last name is incorrect," +
+                " make sure the name contains only letters," +
+                " the first letter is uppercase," +
+                " and the length is greater than 3 and less than 30");
+
+            return false;
+        }
     }
 }
