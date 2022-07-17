2a3c4282fbb17d46b51ab370d85a58c29847970b
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 19:44:40 +0400

    Initial commit: folders and classes

---
+++ UserManagment/UserManagment/DataBase/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagment.DataBase.Models
+{
+    internal class User
+    {
+    }
+}
