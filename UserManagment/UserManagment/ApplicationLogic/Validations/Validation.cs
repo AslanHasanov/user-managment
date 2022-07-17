9641e34dff6eac7b6efd49000837563908be5d0f
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 22:01:35 +0400

    IsValidText method added to Validation class

--- UserManagment/UserManagment/ApplicationLogic/Validations/Validation.cs
+++ UserManagment/UserManagment/ApplicationLogic/Validations/Validation.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserManagment.ApplicationLogic.Validations
 {
     internal class Validation
     {
+        public static bool IsValidText(string text)
+        {
+            string pattern = "(?=^.{3,30}$)([A-Z]+[a-z]+)";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(text))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
