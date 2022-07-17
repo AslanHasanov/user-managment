c7082a3cc90192e2b92a771b4e076b18eee51d6d
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 22:02:56 +0400

    IsConsistEmail method added to Validation class

--- UserManagment/UserManagment/ApplicationLogic/Validations/Validation.cs
+++ UserManagment/UserManagment/ApplicationLogic/Validations/Validation.cs
@@ -20,5 +20,17 @@ namespace UserManagment.ApplicationLogic.Validations
 
             return false;
         }
+
+        public static bool IsConsistEmail(string email)
+        {
+            string pattern = @"^([a-zA-Z0-9]{10,30})(@code\.edu\.az)$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(email))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
