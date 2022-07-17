892cbb4dbd32bc058548544d1d8b77feec92e3d5
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 19:50:59 +0400

    Added propertyes and constrtuctor to the User class

--- UserManagment/UserManagment/DataBase/Models/User.cs
+++ UserManagment/UserManagment/DataBase/Models/User.cs
@@ -8,5 +8,22 @@ namespace UserManagment.DataBase.Models
 {
     internal class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public static int _idCounter = 1;
+        public int Id { get; }
+        public DateTime _registerTime;
+
+        public User(string firstName, string lastName, string email, string password)
+        {
+            Id = _idCounter++;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            _registerTime = DateTime.Now;
+        }
     }
 }
