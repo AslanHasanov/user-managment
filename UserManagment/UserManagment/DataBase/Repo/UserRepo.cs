1337b40ce9422864b86e206cd2052867130a5223
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 21:50:27 +0400

    User Lists and admins details added to UserRepo class

--- UserManagment/UserManagment/DataBase/Repo/UserRepo.cs
+++ UserManagment/UserManagment/DataBase/Repo/UserRepo.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagment.DataBase.Models;
 
 namespace UserManagment.DataBase.Repo
 {
     internal class UserRepo
     {
+        public static List<User> Users { get; set; } = new List<User>()
+        {
+            new User("Admin","Super","admin@gmail.com","123321")
+
+        };
     }
 }
