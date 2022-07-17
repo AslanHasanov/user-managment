033c497a0a54c48d87f7e6bd0b9e4930c4a050fc
AslanHasanov <aslanrh@code.edu.az>
2022-07-17 22:40:42 +0400

    To create UI section in Program class

--- UserManagment/UserManagment/UI/Program.cs
+++ UserManagment/UserManagment/UI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UserManagment.ApplicationLogic;
 
 namespace UserManagment.UI
 {
@@ -6,7 +7,35 @@ namespace UserManagment.UI
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Commands :");
+            Console.WriteLine("/register");
+            Console.WriteLine("/login");
+            Console.WriteLine("/exit");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Enter command : ");
+                string command = Console.ReadLine();
+
+                if (command == "/register")
+                {
+                    Authentication.Register();
+                }
+                else if (command == "/login")
+                {
+                    Authentication.Login();
+                }
+
+                else if (command == "/exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Command not found!");
+                }
+            }
         }
     }
 }
